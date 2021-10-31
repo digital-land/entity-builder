4edd879423b43786f223f7f7c9c4cc8052054189
Paul Downey <psd@whatfettle.com>
2021-10-31 17:59:13 +0000

    Migrate organisations

--- bin/organisation.py
+++ bin/organisation.py
@@ -11,19 +11,33 @@ import geojson
 import shapely.wkt
 
 
+# organisation responsible for creating, changing or disolving the entity
+# TBD: move to organisation-collection
+dataset_organisation = {
+    "national-park-authority": "government-organisation:D7",
+}
+organisation_organisation = {
+    "development-corporation:Q20648596": "local-authority-eng:GLA",
+    "development-corporation:Q6670544": "local-authority-eng:GLA",
+}
+
+
 fieldnames = set()
 organisations = {}
 
 for row in csv.DictReader(open("var/cache/organisation.csv")):
     organisation = row["organisation"]
-    del row["organisation"]
 
     dataset = organisation.split(":")[0]
     reference = organisation.split(":")[1]
 
     row["dataset"] = dataset
-    row[dataset] = reference
+    row["prefix"] = dataset
     row["reference"] = reference
+
+    row["organisation"] = organisation_organisation.get(
+        organisation, dataset_organisation.get(dataset, "")
+    )
 
     fieldnames = fieldnames.union(set(row.keys()))
     organisations[organisation] = row
