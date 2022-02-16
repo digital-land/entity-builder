fd2ac71a3289738fb51bdf6f09a788691e3427b6
Toby Petty <tobypetty1@gmail.com>
2022-02-16 16:43:44 +0000

    Repoint download.sh at digital-land-production-collection-dataset
    To be merged once digital-land-production-collection-dataset is canonical dataset

--- bin/download.sh
+++ bin/download.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-s3="https://collection-dataset.s3.eu-west-2.amazonaws.com/"
+s3="https://digital-land-production-collection-dataset.s3.eu-west-2.amazonaws.com/"
 
 dir=var/dataset/
 mkdir -p $dir
@@ -13,7 +13,7 @@ csvcut -c dataset,collection specification/dataset.csv |
     while read dataset collection
 do
     # current s3 structure has collection, but should be flattend
-    # https://collection-dataset.s3.eu-west-2.amazonaws.com/{COLLECTION}-collection/issue/{DATASET}/{DATASET}.sqlite3
+    # https://*-collection-dataset.s3.eu-west-2.amazonaws.com/{COLLECTION}-collection/issue/{DATASET}/{DATASET}.sqlite3
     case "$collection" in
     ""|organisation) continue ;;
     esac
